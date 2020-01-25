c216fb981d7afcd5ef1748bb95900fe1e48f78cc
lugovoyNikita <zenit27069621@mail.ru>
2020-01-26 02:10:00 +0300

    initial commit

---
+++ app/src/main/java/com/example/homework_whatsapp_constraintlayout/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.homework_whatsapp_constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMyButton(View view) {
+        TextView textView = findViewById(R.id.myTextViewMassage);
+        EditText editText = findViewById(R.id.myEditText);
+
+        String textFromEditText = editText.getText().toString();
+
+        textView.setText(textFromEditText);
+    }
+}
